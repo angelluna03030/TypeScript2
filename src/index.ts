02f16678d282bad31e197afd167fcea64f3c531f
angel luna <angelstivengarcialuna@gmail.com>
2024-10-02 16:50:25 -0500

    Terminar la primero clase

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import type { addEmitHelpers } from "typescript";
+
 function suma(a: number, b: number): number {
   return a + b;
 }
@@ -54,3 +56,145 @@ const sayHi = (name: string) => {
 };
 
 soyUnaFromFunction(sayHi);
+
+
+
+const rest =(a: number, b: number) =>{
+  return a - b 
+}
+
+//esto no va devolver nada 
+//never 
+function throwError(message:string): never {
+  throw new Error(message);
+}
+
+
+
+
+function logMassege(message:string): void  {
+  console.log(message)
+}
+
+// const avengers = ["Hulk", "Tony Start"]
+
+
+// avengers.forEach(a=>{
+//   console.log(a.toString())
+// })
+
+
+
+let hero = {
+  name : "thor",
+  age: 36,
+  power: 1500
+}
+
+const createhero = (name: string, age: number)=>{
+  return{
+    name, age
+  }
+
+}
+
+
+const thor = createhero('thor',20)
+
+
+type Hero = {
+  name: string,
+  age:number
+}
+
+
+const address ={
+  planet: "Earth",
+  city : "colombia"
+} 
+
+type Address = typeof address
+
+
+const addressTwitch : Address = {
+  planet :"Tierrar",
+  city: "colombia"
+}
+//type from funtion return 
+
+function createAddress() {
+  return{
+    planet: "Tierra",
+    city : "colombia"
+  }
+}
+type Addres = ReturnType<typeof createAddress>
+
+
+
+//Arrays 
+
+
+
+
+const languages:(string | number)[]= [] 
+
+
+
+
+// languages.push("hola mundo")
+// languages.push(4)
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+// languages.push("hola mundo")
+
+/*
+['X', 'O' , 'x'],
+['X', 'O' , 'x'],
+['X', 'O' , 'x'],
+
+
+*/
+
+
+type CellValue = 'X' | 'O' | ''
+//tuplas
+type GameBoard = [
+  [CellValue, CellValue, CellValue],
+  [CellValue, CellValue, CellValue],
+  [CellValue, CellValue, CellValue]
+]
+const gameBoard: CellValue [][]=[
+  ['X', 'O' , 'X', ],
+  ['X', 'O' , 'X'],
+  ['X', 'O' , 'X']  
+
+]
+
+//Tuplas 
+// type State = [string, (name: string) =>void]
+// const [ hero, setHero] =State("thor")
+//type RGB = [number, number, number ] // 0 - 255
+//const rgb : RGB = [ 2,3,4 ]
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
